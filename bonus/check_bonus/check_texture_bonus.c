4f675467e848ecb9bc6a2e45fa436ae67fbc110b
Mohaben-1 <mohammedwhb2001@gmail.com>
2025-07-04 16:48:07 +0100

    parse bonus

--- bonus/check_bonus/check_texture_bonus.c
+++ bonus/check_bonus/check_texture_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mohaben- <mohaben-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 16:12:13 by mohaben-          #+#    #+#             */
-/*   Updated: 2025/07/03 17:51:42 by mohaben-         ###   ########.fr       */
+/*   Updated: 2025/07/04 16:47:07 by mohaben-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ int	check_texture(char *texture)
 		return (0);
 	if (!ft_strncmp(texture, "tx:", 3))
 		texture += 3;
+	else
+	{
+		ft_putstr_fd("Error\nInvalid texture identifier: Only 'tx:' is accepted.\n", 2);
+		return (0);
+	}
 	len = ft_strlen(texture);
 	if (len < 4 || ft_strncmp(texture + len - 4, ".xpm", 4) != 0)
 	{
